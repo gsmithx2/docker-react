1a1ca57378d130234817eb92d8e0c222e385f843
Greg Smith <gsmith@radiantlogic.com>
2024-08-26 15:42:49 -0400

    added travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hi dere! I made a change!
+          Edit <code>src/App.js</code> and save to reload. Hi dere!
         </p>
         <a
           className="App-link"
