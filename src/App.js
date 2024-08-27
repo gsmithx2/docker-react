dbe7ea9094832d47554160a772d64e557044089f
Greg Smith <gsmith@radiantlogic.com>
2024-08-27 17:17:09 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hi dere!
+          Edit <code>src/App.js</code> and save to reload. Hi dere! Changed again.
         </p>
         <a
           className="App-link"
